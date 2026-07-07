775a529dcfcea44794c602eb7181b46dcc0ed70d
tinyblinker <2149934895@qq.com>
2026-07-08 00:19:22 +0800

    fish functions

--- home.nix
+++ home.nix
@@ -19,6 +19,16 @@
     shellAliases = {
       btw = "echo i use nixos, btw";
     };
+    functions = {
+      y = ''
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+          builtin cd -- "$cwd"
+        end
+        command rm -f -- "$tmp"
+      '';
+    };
   };
   programs.git = {
     enable = true;
