e998c37f6acca38e7901114842a9f5ce63168ad9
tinyblinker <2149934895@qq.com>
2026-07-11 12:51:03 +0800

    change nvim config

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -8,13 +8,7 @@ return {
 		{
 			"L3MON4D3/LuaSnip",
 			version = "2.*",
-			build = (function()
-				-- 排除win32环境
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
+			build = "make install_jsregexp",
 			dependencies = {
 				{
 					"rafamadriz/friendly-snippets",
