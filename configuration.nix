445d3b53a1ba4920aef045a00c2777e684bf7cb0
tinyblinker <2149934895@qq.com>
2026-07-11 09:50:19 +0800

    change substituor

--- configuration.nix
+++ configuration.nix
@@ -132,7 +132,6 @@
   nix.settings.substituters = [
     "https://mirrors.ustc.edu.cn/nix-channels/store"
     "https://mirrors.cernet.edu.cn/nix-channels/store"
-    "https://mirrors.sjtu.edu.cn/nix-channels/store"
     "https://mirrors.bfsu.edu.cn/nix-channels/store"
     "https://mirrors.nju.edu.cn/nix-channels/store"
     "https://mirrors.iscas.ac.cn/nix-channels/store"
