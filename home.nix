dc33596c2e2e86279713a0e9400f8b85917f06fc
tinyblinker <2149934895@qq.com>
2026-07-11 14:11:05 +0800

    111

--- home.nix
+++ home.nix
@@ -236,6 +236,7 @@
       cursor_trail = "3";
       cursor_trail_decay = "0.1 0.4";
       cursor_trail_start_threshold = "2";
+      background_opacity = 0.8;
     };
     keybindings = {
       "ctrl+shift+enter" = "no_op";
