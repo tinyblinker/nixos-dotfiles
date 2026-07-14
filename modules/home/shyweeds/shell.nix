835d4d4773142601a234cddb5d1b2a808457b498
tinyblinker <2149934895@qq.com>
2026-07-14 20:41:09 +0800

    add devshell

--- modules/home/shyweeds/shell.nix
+++ modules/home/shyweeds/shell.nix
@@ -1,21 +1,30 @@
 {
-  programs.opencode.enable = true;
-  programs.bash.enable = true;
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = "fish_vi_key_bindings";
-    shellAliases = {
-      btw = "echo i use nixos, btw";
+  programs = {
+    direnv = {
+      enable = true;
+      enableFishIntegration = true;
+      # NOTE: let nix store the evalutions in the dirs
+      # without being cleared by the "nix-collect-garbage"
+      nix-direnv.enable = true;
     };
-    functions = {
-      y = ''
-        set tmp (mktemp -t "yazi-cwd.XXXXXX")
-        command yazi $argv --cwd-file="$tmp"
-        if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
-          builtin cd -- "$cwd"
-        end
-        command rm -f -- "$tmp"
-      '';
+    opencode.enable = true;
+    bash.enable = true;
+    fish = {
+      enable = true;
+      interactiveShellInit = "fish_vi_key_bindings";
+      shellAliases = {
+        btw = "echo i use nixos, btw";
+      };
+      functions = {
+        y = ''
+          set tmp (mktemp -t "yazi-cwd.XXXXXX")
+          command yazi $argv --cwd-file="$tmp"
+          if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+            builtin cd -- "$cwd"
+          end
+          command rm -f -- "$tmp"
+        '';
+      };
     };
   };
 }
