b11d2a15faa10fc4121cb8cc8d0be23e64bea731
tinyblinker <2149934895@qq.com>
2026-07-14 18:52:39 +0800

    bugfix

--- modules/home/shyweeds/desktop/swaync.nix
+++ modules/home/shyweeds/desktop/swaync.nix
@@ -2,22 +2,7 @@
   services.swaync = {
     enable = true;
     style = "/home/shyweeds/dotfiles/config/matugen/output/swaync.css";
-    settings = {
-      positionX = "right";
-      positionY = "top";
-      control-center-margin-top = 8;
-      control-center-margin-bottom = 8;
-      control-center-margin-right = 8;
-      control-center-margin-left = 8;
-      control-center-height = 600;
-      control-center-width = 400;
-      timeout = 10;
-      timeout-low = 7;
-      timeout-critical = 0;
-      fit-to-screen = true;
-      image-visibility = "when-available";
-      notification-window-width = 400;
-      keyboard-shortcuts = true;
-    };
   };
+
+  xdg.configFile."swaync/config.json".source = ../../../../config/swaync/config.json;
 }
