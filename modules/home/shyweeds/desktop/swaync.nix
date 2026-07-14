624981e0669e76698fb3fecec7537558cc6e5206
tinyblinker <2149934895@qq.com>
2026-07-14 19:09:47 +0800

    bugfix

--- modules/home/shyweeds/desktop/swaync.nix
+++ modules/home/shyweeds/desktop/swaync.nix
@@ -1,9 +1,17 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   services.swaync = {
     enable = true;
-    style = "/home/shyweeds/dotfiles/config/matugen/output/swaync.css";
+    style = null;
   };
+
+  xdg.configFile."swaync/style.css" = lib.mkForce {
+    enable = false;
+  };
+
+  home.file.".config/swaync/style.css".source =
+    config.lib.file.mkOutOfStoreSymlink
+      "/home/shyweeds/dotfiles/config/matugen/output/swaync.css";
 
   xdg.configFile."swaync/config.json".source =
     lib.mkForce ../../../../config/swaync/config.json;
