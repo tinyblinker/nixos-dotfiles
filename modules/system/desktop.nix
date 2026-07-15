c593fad62e09b85ac1272bf2b7a1b79858c8dde4
tinyblinker <2149934895@qq.com>
2026-07-15 19:20:03 +0800

    correct the portal

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -6,7 +6,7 @@
     enable = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-wlr
+      pkgs.xdg-desktop-portal-gnome
     ];
   };
   security.pam.services.hyprlock = { };
